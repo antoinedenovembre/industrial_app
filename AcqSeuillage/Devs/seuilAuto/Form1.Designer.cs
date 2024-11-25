c5f790edbd209247fb0d37063284e5fe0f2b169d
COUCOU <vincent.bougeon@yahoo.fr>
2024-11-25 14:31:00 +0100

    COUCOU envoie le projet

---
+++ AcqSeuillage/Devs/seuilAuto/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace seuilAuto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ouvrirImage = new System.Windows.Forms.OpenFileDialog();
+            this.imageDepart = new System.Windows.Forms.PictureBox();
+            this.buttonOuvrir = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.imageSeuillee = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.valeurSeuilAuto = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_objetA = new System.Windows.Forms.TextBox();
+            this.txt_objetB = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.imageDepart)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageSeuillee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ouvrirImage
+            // 
+            this.ouvrirImage.Filter = "Tous Fichiers | *.*";
+            this.ouvrirImage.InitialDirectory = "C:\\Hubert_KONIK\\Recherche\\Database\\Images";
+            // 
+            // imageDepart
+            // 
+            this.imageDepart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.imageDepart.Cursor = System.Windows.Forms.Cursors.Default;
+            this.imageDepart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageDepart.ImageLocation = "";
+            this.imageDepart.Location = new System.Drawing.Point(0, 0);
+            this.imageDepart.Name = "imageDepart";
+            this.imageDepart.Size = new System.Drawing.Size(400, 400);
+            this.imageDepart.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageDepart.TabIndex = 0;
+            this.imageDepart.TabStop = false;
+            // 
+            // buttonOuvrir
+            // 
+            this.buttonOuvrir.Location = new System.Drawing.Point(12, 12);
+            this.buttonOuvrir.Name = "buttonOuvrir";
+            this.buttonOuvrir.Size = new System.Drawing.Size(104, 34);
+            this.buttonOuvrir.TabIndex = 1;
+            this.buttonOuvrir.Text = "Start";
+            this.buttonOuvrir.UseVisualStyleBackColor = true;
+            this.buttonOuvrir.Click += new System.EventHandler(this.buttonOuvrir_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.imageDepart);
+            this.panel1.Location = new System.Drawing.Point(12, 122);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 400);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.imageSeuillee);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(477, 122);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(400, 400);
+            this.panel2.TabIndex = 3;
+            // 
+            // imageSeuillee
+            // 
+            this.imageSeuillee.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageSeuillee.Location = new System.Drawing.Point(0, 0);
+            this.imageSeuillee.Name = "imageSeuillee";
+            this.imageSeuillee.Size = new System.Drawing.Size(400, 400);
+            this.imageSeuillee.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageSeuillee.TabIndex = 1;
+            this.imageSeuillee.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(0, 0);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // valeurSeuilAuto
+            // 
+            this.valeurSeuilAuto.Location = new System.Drawing.Point(635, 93);
+            this.valeurSeuilAuto.Name = "valeurSeuilAuto";
+            this.valeurSeuilAuto.ReadOnly = true;
+            this.valeurSeuilAuto.Size = new System.Drawing.Size(62, 20);
+            this.valeurSeuilAuto.TabIndex = 0;
+            this.valeurSeuilAuto.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.valeurSeuilAuto.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(474, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Valeur seuillage : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(474, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombre d\'objets A détectés :";
+            // 
+            // txt_objetA
+            // 
+            this.txt_objetA.Location = new System.Drawing.Point(635, 20);
+            this.txt_objetA.Name = "txt_objetA";
+            this.txt_objetA.ReadOnly = true;
+            this.txt_objetA.Size = new System.Drawing.Size(62, 20);
+            this.txt_objetA.TabIndex = 6;
+            this.txt_objetA.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_objetA.Visible = false;
+            // 
+            // txt_objetB
+            // 
+            this.txt_objetB.Location = new System.Drawing.Point(635, 49);
+            this.txt_objetB.Name = "txt_objetB";
+            this.txt_objetB.ReadOnly = true;
+            this.txt_objetB.Size = new System.Drawing.Size(62, 20);
+            this.txt_objetB.TabIndex = 8;
+            this.txt_objetB.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txt_objetB.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Nombre d\'objets B détectés :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 505);
+            this.Controls.Add(this.txt_objetB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_objetA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.valeurSeuilAuto);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.buttonOuvrir);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Seuillage en deux classes";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Close);
+            ((System.ComponentModel.ISupportInitialize)(this.imageDepart)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageSeuillee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog ouvrirImage;
+        private System.Windows.Forms.PictureBox imageDepart;
+        private System.Windows.Forms.Button buttonOuvrir;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imageSeuillee;
+        private System.Windows.Forms.TextBox valeurSeuilAuto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_objetA;
+        private System.Windows.Forms.TextBox txt_objetB;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
